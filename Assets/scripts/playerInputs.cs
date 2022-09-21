9a992d87d398634efeee2a47821d4f74eab0a471
ThatGuyXD <chickenbacon97@gmail.com>
2022-09-22 00:09:43 +1000

    added materials folder and a scripts folder with a basic player movement and camera movement script

---
+++ Assets/scripts/playerInputs.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInputs : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 1.0f;
+    [SerializeField] float jumpStrength = 2.0f;
+    // [SerializeField] float maxSpeed = 10.0f;
+    new Rigidbody rigidbody;
+
+    private void Awake() 
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void start() 
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = rigidbody.velocity.magnitude;
+        if(Input.GetKey(KeyCode.A)) {
+            rigidbody.position += Vector3.left * this.playerSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.S)) {
+            rigidbody.position += Vector3.back * this.playerSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.D)) {
+            rigidbody.position += Vector3.right * this.playerSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.W)) {
+            rigidbody.position += Vector3.forward * this.playerSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            rigidbody.velocity += Vector3.up * this.jumpStrength;
+        }
+
+    }
+}
