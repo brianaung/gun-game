7f7f8d80d70a9997a388408157a21186c7b06f32
Brian Aung <soekhantaung16@gmail.com>
2022-10-05 20:14:24 +1100

    fix file names mismatch

--- Assets/Code/Scripts/WeaponSwitching.cs
+++ Assets/Code/Scripts/WeaponSwitching.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class weaponSwiching : MonoBehaviour
+public class WeaponSwitching : MonoBehaviour
 {
     //[Header("reference")]
     [Header("reference")]
