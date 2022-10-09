05a0ae4e683329a5cac7bd33fe1bd219d5d553fa
Brian Aung <soekhantaung16@gmail.com>
2022-10-09 17:29:27 +1100

    set up dungeon creator

---
+++ Assets/Code/Scripts/Dungeon Generation/DungeonCreator.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonCreator : MonoBehaviour
+{
+    public int dungeonWidth, dungeonLength;
+    public int roomWidthMin, roomLengthMin;
+    public int maxIterations;
+    public int corridorWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateDungeon();
+    }
+
+    private void CreateDungeon()
+    {
+        DungeonGenerator generator = new DungeonGenerator(dungeonWidth, dungeonLength);
+        var listOfRooms = generator.CalculateRooms(maxIterations, roomWidthMin, roomLengthMin);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Code/Scripts/Dungeon Generation/DungeonGenerator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator
+{
+    List<RoomNode> allSpaceNodes = new List<RoomNode>;
+    private int dungeonWidth;
+    private int dungeonLength;
+
+    public DungeonGenerator(int dungeonWidth, int dungeonLength)
+    {
+        this.dungeonWidth = dungeonWidth;
+        this.dungeonLength = dungeonLength;
+    }
+
+    public List<Node> CalculateRooms(int maxIterations, int roomWidthMin, int roomLengthMin)
+    {
+        // implement Binary Space Partitioner algo
+        BinarySpacePartitioner bsp = 
+            new BinarySpacePartitioner(this.dungeonWidth, this.dungeonLength);
+
+        allSpaceNodes = bsp.PrepareNodesCollection(maxIterations, roomWidthMin, roomLengthMin);
+    }
+
+
+}
