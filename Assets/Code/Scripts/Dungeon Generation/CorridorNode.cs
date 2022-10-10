8d11a3eb216467294451fbdedd7dd83e1a463abb
Brian Aung <soekhantaung16@gmail.com>
2022-10-10 20:34:24 +1100

    implement right left corridor connection

--- Assets/Code/Scripts/Dungeon Generation/CorridorNode.cs
+++ Assets/Code/Scripts/Dungeon Generation/CorridorNode.cs
@@ -23,6 +23,102 @@ public class CorridorNode : Node
     {
         var relativePosStruct2 = CheckPos();
 
+        switch (relativePosStruct2)
+        {
+            case RelativePos.Up:
+                processUpDown(this.struct1, this.struct2);
+                break;
+            case RelativePos.Down:
+                processUpDown(this.struct2, this.struct1);
+                break;
+            case RelativePos.Right:
+                processRightLeft(this.struct1, this.struct2);
+                break;
+            case RelativePos.Left:
+                processRightLeft(this.struct2, this.struct1);
+                break;
+            default:
+                break;
+        };
+    }
+
+    private void processRightLeft(Node struct1, Node struct2)
+    {
+        Node leftStruct = null;
+        List<Node> leftStructChildren = StructureHelper.TraverseGraphs(struct1);
+
+        Node rightStruct = null;
+        List<Node> rightStructChildren = StructureHelper.TraverseGraphs(struct2);
+
+        // sort by most right aligned structs in left structs list
+        var sortedLeft = leftStructChildren.OrderByDescending(child => child.TopRightAreaCorner.x).ToList();
+
+        if (sortedLeft.Count == 1)
+        { // no children
+            leftStruct = sortedLeft[0];
+        } else
+        {
+            int maxX = sortedLeft[0].TopRightAreaCorner.x;
+            sortedLeft = 
+                sortedLeft.Where(children => Math.Abs(maxX - children.TopRightAreaCorner.x) < 10).ToList();
+
+            int index = Random.Range(0, sortedLeft.Count);
+            leftStruct = sortedLeft[index];
+        }
+
+        var possibleRightList = rightStructChildren.Where(
+           child => GetValidY(
+               leftStruct.TopRightAreaCorner,
+               leftStruct.BottomRightAreaCorner,
+               child.TopLeftAreaCorner,
+               child.BottomLeftAreaCorner
+               ) != -1
+           ).OrderBy(child => child.BottomRightAreaCorner.x).ToList();
+
+        if (possibleRightList.Count <= 0)
+        {
+            rightStruct = struct2;
+        }
+        else
+        {
+            rightStruct = possibleRightList[0];
+        }
+        int y = GetValidY(leftStruct.TopLeftAreaCorner, leftStruct.BottomRightAreaCorner,
+            rightStruct.TopLeftAreaCorner,
+            rightStruct.BottomLeftAreaCorner);
+        while (y == -1 && sortedLeft.Count > 1)
+        {
+            sortedLeft = sortedLeft.Where(
+                child => child.TopLeftAreaCorner.y != leftStruct.TopLeftAreaCorner.y).ToList();
+            leftStruct = sortedLeft[0];
+            y = GetValidY(leftStruct.TopLeftAreaCorner, leftStruct.BottomRightAreaCorner,
+            rightStruct.TopLeftAreaCorner,
+            rightStruct.BottomLeftAreaCorner);
+        }
+        BottomLeftAreaCorner = new Vector2Int(leftStruct.BottomRightAreaCorner.x, y);
+        TopRightAreaCorner = new Vector2Int(rightStruct.TopLeftAreaCorner.x, y + this.corridorWidth);
+    }
+
+    private int GetValidY(Vector2Int leftNodeUp, Vector2Int leftNodeDown, Vector2Int rightNodeUp, Vector2Int rightNodeDown)
+    {
+        if (rightNodeUp.y >= leftNodeUp.y && leftNodeDown.y >= rightNodeDown.y)
+        {
+        }
+        if (rightNodeUp.y <= leftNodeUp.y && leftNodeDown.y <= rightNodeDown.y)
+        {
+        }
+        if (leftNodeUp.y >= rightNodeDown.y && leftNodeUp.y <= rightNodeUp.y)
+        {
+        }
+        if (leftNodeDown.y >= rightNodeDown.y && leftNodeDown.y <= rightNodeUp.y)
+        {
+        }
+        return -1;
+    }
+
+    private void processUpDown(Node struct1, Node struct2)
+    {
+        throw new NotImplementedException();
     }
 
     private RelativePos CheckPos()
