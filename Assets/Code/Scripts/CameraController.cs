aa75306b79bef19c3ed7ac73dfba146c2a410542
ThatGuyXD <chickenbacon97@gmail.com>
2022-10-12 17:21:01 +1100

    changed how camera collision works

--- Assets/Code/Scripts/CameraController.cs
+++ Assets/Code/Scripts/CameraController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    public GameObject cameraCollider;
     public Camera cam;
     public float yOffset = 1.0f;
     public float zOffset = -5.0f;
@@ -16,13 +15,15 @@ public class CameraController : MonoBehaviour
     private Vector3 currRotation;
     private Vector3 camVelocity = Vector3.zero;
     private float smoothTime = 0.1f;
-    private float sphereCastRadius = 1f;
-
-    private RaycastHit hit;
-    private float cameraCollisionOffset = 1f;
+    private float cameraCollisionOffset = 2.5f;
+    private float maxDist = 4f;
+    private float playerCamDist;
     private void Awake() 
     {
         playerTransform = player.transform;
+        transform.position = playerTransform.position - (transform.forward * -zOffset) + (transform.up * yOffset);
+        playerCamDist = Vector3.Distance(playerTransform.position, transform.position);
+        
     }
 
     public void cameraMove()
@@ -40,20 +41,28 @@ public class CameraController : MonoBehaviour
         currRotation = Vector3.SmoothDamp(currRotation, nextRotation, ref camVelocity, smoothTime);
 
         transform.localEulerAngles = currRotation;
-        transform.position = playerTransform.position - (transform.forward * -zOffset) + (transform.up * yOffset);
 
-        // Basic camera collision. Simply checks if there is anything between the player and the camera. 
-        // if there is, the camera will move to the hit point of the linecast between the player and camera
-        // todo fix camera collision
-        if(Physics.Linecast(transform.position, transform.position - (transform.forward * cameraCollisionOffset), out hit))
+        
+
+        // transform.position = playerTransform.position - (transform.forward * -zOffset) + (transform.up * yOffset);
+
+        // Basic camera collision. Checks if there is object behind the camera. if there is set the transform of the camera to that hit point minus 
+        // some offset. There is another if statement to check if the player is too far away from the camera as the camera will get stuck to the wall.
+        // Although this method works, it will sometimes still clip through a wall when moving the camera at some weird angles.
+        if(Physics.Raycast(transform.position, -transform.forward, out var hit, maxDist))
         {
-            Debug.Log(hit.point);
-            cam.transform.position = hit.point;
-            var localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y, cam.transform.localPosition.z + cameraCollisionOffset);
-            cam.transform.localPosition = localPosition;
-            
+            transform.position = hit.point - (transform.forward * -cameraCollisionOffset);
+            if(Vector3.Distance(transform.position, playerTransform.position) > playerCamDist)
+            {
+                transform.position = playerTransform.position - (transform.forward * -zOffset) + (transform.up * yOffset);
+            }
         }
 
+        else
+        {
+            transform.position = playerTransform.position - (transform.forward * -zOffset) + (transform.up * yOffset);
+        }
+        
     }
 }
 
