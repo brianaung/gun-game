34329d14d9ea181cb00d85e178ff5ea2ace95b90
woohyeokj <woohyeokj@student.unimelb.edu.au>
2022-10-17 17:19:57 +0900

    role added
    added role to prevent Boss destroyed after an contact

--- Assets/Code/Scripts/Enemy/ContactController.cs
+++ Assets/Code/Scripts/Enemy/ContactController.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class ContactController : MonoBehaviour
 {
     [SerializeField] private ParticleSystem collisionParticles;
-    [SerializeField] private string tagToDamage;
     // [SerializeField] private int damageAmount = 50;
+    [SerializeField] private string tagToDamage;
+    [SerializeField] private string Role;
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == this.tagToDamage)
         {
-            
             var particles = Instantiate(this.collisionParticles);
             particles.transform.position = transform.position;
             
-            Destroy(gameObject);
+            if(Role != "boss") Destroy(gameObject);
         }
     }
 }
