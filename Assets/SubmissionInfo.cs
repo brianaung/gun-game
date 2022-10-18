c9113609ff235b20effb024965cfd94933edf89d
Brian Aung <soekhantaung16@gmail.com>
2022-10-18 23:54:33 +1100

    add yt link

--- Assets/SubmissionInfo.cs
+++ Assets/SubmissionInfo.cs
@@ -37,7 +37,7 @@ public static class SubmissionInfo
     // By the gameplay video milestone deadline this should be a direct link
     // to a YouTube video upload containing your video. Ensure "Made for kids"
     // is turned off in the video settings. 
-    public static readonly string GameplayVideo = "https://youtube.com/...";
+    public static readonly string GameplayVideo = "https://youtu.be/aUSfDsHUbT0";
     
     // No more info to fill out!
     // Please don't modify anything below here.
