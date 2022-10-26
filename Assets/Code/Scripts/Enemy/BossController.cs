3f4fe32b6cbe2e8f39a9bff929f25a9cf0cdaab7
ThatGuyXD <chickenbacon97@gmail.com>
2022-10-26 23:36:27 +1100

    fixed ui

---
+++ Assets/Code/Scripts/Enemy/BossController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class BossController : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem deathEffect;
+    private MeshRenderer _renderer;
+
+
+
+    private void Awake()
+    {
+        this._renderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    // Same as above, but listens to onDeath events.
+    public void Kill()
+    {
+        var particles = Instantiate(this.deathEffect);
+        particles.transform.position = -transform.position;
+
+        GameManager.Instance.playAgain();
+    }
+}
